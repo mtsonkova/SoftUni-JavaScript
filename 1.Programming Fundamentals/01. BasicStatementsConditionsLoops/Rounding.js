3bdccad461814dea99f2f7fe1c24b1d33e41b7dc
mtsonkova <mtsonkova950@gmail.com>
2024-06-09 22:25:29 +0300

    In progress

--- 1.Programming Fundamentals/01. BasicStatementsConditionsLoops/Rounding.js
+++ 1.Programming Fundamentals/01. BasicStatementsConditionsLoops/Rounding.js
@@ -3,5 +3,8 @@ function solveRounding(num, round) {
         round = 15;
     }
     let roundedNumber = num.toFixed(round);
-    console.log(roundedNumber.parseFloat());
+    console.log(parseFloat(roundedNumber));
 }
+
+solveRounding(3.1415926535897932384626433832795,2);
+solveRounding(10.5,3);
