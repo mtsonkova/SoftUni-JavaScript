33a5da518e6b64e80875ae26e0334daa5f4d797d
mtsonkova <mtsonkova950@gmail.com>
2024-06-11 21:41:37 +0300

    In progress

---
+++ 1.Programming Fundamentals/02. DataTypesAndVariables/2. NumsToString.js
@@ -0,0 +1,8 @@
+function solveCharsToString(first, second, third) {
+    console.log(`${first}${second}${third}`);
+}
+
+solveCharsToString('a',
+'b',
+'c'
+)
---
+++ 1.Programming Fundamentals/02. DataTypesAndVariables/3. TownInfo.js
@@ -0,0 +1,9 @@
+function solveTownInfo (name, population, area){
+    console.log(`Town ${name} has population of ${population} and area ${area} square km.`)
+}
+
+solveTownInfo('Sofia',
+1286383,
+492
+)
+
