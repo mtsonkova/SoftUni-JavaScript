2610a41a6f71c85c441c62e93bdda3f72302da12
mtsonkova <mtsonkova950@gmail.com>
2024-06-11 03:37:26 +0300

    In progress

--- 1.Programming Fundamentals/01. BasicStatementsConditionsLoops/10. KingJosersPyramid.js
+++ 1.Programming Fundamentals/01. BasicStatementsConditionsLoops/10. KingJosersPyramid.js
@@ -6,8 +6,10 @@ function solveKingJoserPyramid(base, increment) {
     let gold = 0;
     let stepArea = 0;
     let stepPerimeter = 0;
+    let totalArea = 0;
 
 
+    /*
     for (let i = base; i > 0; i -= 2) {
         stepCounter++;
         let current = i;
@@ -26,19 +28,42 @@ function solveKingJoserPyramid(base, increment) {
                 marbles += stepPerimeter;
             }
         }
+    }
 
+    */
 
+    for (let i = base; i > 0; i -= 2) {
+        stepCounter++;
+        let current = i;
+        if (i - 2 <= 0) {
+            gold = current * current;
+        } else {
+            totalArea = current * current * increment;
+            stepPerimeter = ((current * 4) - 4) * increment;
+            stones += (totalArea - stepPerimeter);
+
+            if (stepCounter % 5 === 0) {
+                lapisLazuli += stepPerimeter;
+            } else {
+                marbles += stepPerimeter;
+            }
+        }
 
     }
+
+    let finalHeight = Math.floor(stepCounter * increment);
+
     console.log(`Stoens required: ${Math.ceil(stones)}`);
     console.log(`Marble required: ${Math.ceil(marbles)}`);
     console.log(`Lapis Lazuli required: ${Math.ceil(lapisLazuli)}`);
     console.log(`Gold required: ${Math.ceil(gold)}`);
-    console.log(`Final pyramid height: ${Math.floor(stepCounter * increment)}`);
+    console.log(`Final pyramid height: ${finalHeight}`);
+
+
 
 }
 
 //solveKingJoserPyramid(11, 1);
-solveKingJoserPyramid(11, 0.75);
-//solveKingJoserPyramid(12, 1);
-//solveKingJoserPyramid(23, 0.5);
+//solveKingJoserPyramid(11, 0.75);
+ //solveKingJoserPyramid(12, 1);
+solveKingJoserPyramid(23, 0.5);
