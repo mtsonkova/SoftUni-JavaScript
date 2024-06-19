0bee1cc686cad9ed527da147dbabb62012988ab0
mtsonkova <mtsonkova950@gmail.com>
2024-06-20 00:46:28 +0300

    In progress

--- 2. Front End Technologies Basics and JS Advanced/18.Practical-Project-Automated-Testing-of-Single-Page-App-with-qUnit-Solution/tests/QUnit_tests/integration.test.js
+++ 2. Front End Technologies Basics and JS Advanced/18.Practical-Project-Automated-Testing-of-Single-Page-App-with-qUnit-Solution/tests/QUnit_tests/integration.test.js
@@ -10,9 +10,9 @@ let userId = '';
 QUnit.config.reorder = false;
 
 QUnit.module('user functionalities', () => {
-    QUnit.test('regustration', async(assert) => {
+    QUnit.test('regustration', async (assert) => {
         let path = 'users/register';
-        
+
         let random = Math.floor(Math.random() * 10000);
         let email = `abv${random}@abv.bg`;
 
@@ -21,7 +21,7 @@ QUnit.module('user functionalities', () => {
         let response = await fetch(baseUrl + path, {
             method: 'POST',
             headers: {
-                'content-type' : 'application/json'
+                'content-type': 'application/json'
             },
 
 
@@ -29,8 +29,8 @@ QUnit.module('user functionalities', () => {
         });
 
         let json = await response.json();
-        
-        
+
+
         assert.ok(response.ok, 'successfull response');
         //assert email
         assert.ok(json.hasOwnProperty('email'), 'email exist');
@@ -57,19 +57,19 @@ QUnit.module('user functionalities', () => {
         sessionStorage.setItem('game-user', JSON.stringify(user)); //set token to session store in browser
     });
 
-    QUnit.test('login', async(assert) => {
+    QUnit.test('login', async (assert) => {
         let path = 'users/login';
         let response = await fetch(baseUrl + path, {
-            method : 'POST',
+            method: 'POST',
             headers: {
-                'content-type' : 'application/json'
+                'content-type': 'application/json'
             },
             body: JSON.stringify(user)
         });
 
         let json = await response.json();
         assert.ok(response.ok, 'successfull response');
-       
+
         //assert email
         assert.ok(json.hasOwnProperty('email'), 'email exist');
         assert.equal(json['email'], user.email, 'expexted mail');
@@ -103,7 +103,7 @@ QUnit.module('user functionalities', () => {
 });
 
 QUnit.module('game functionalities', () => {
-    QUnit.test('get all games', async(assert) => {
+    QUnit.test('get all games', async (assert) => {
         let path = 'data/games';
         let queryParams = '?sortBy=_createdOn%20desc';
 
@@ -113,25 +113,42 @@ QUnit.module('game functionalities', () => {
         assert.ok(response.ok, 'successfull response');
         assert.true(Array.isArray(json), 'Response is an Array of objects');
 
-        for(let i = 0; i < json.lenght - 1; i++) {
+        json.forEach(jsonProperty => {
 
             // _ownerId
-            assert.ok(json.hasOwnProperty('_ownerId'), 'Property _ownerId exists.');
-            assert.strictEqual(json['_ownerId'], 'string', 'Property _ownerId is string.');
-            assert.true(json['_userId'].length > 0);
-        
+            assert.ok(jsonProperty.hasOwnProperty('_ownerId'), 'Property _ownerId exists.');
+            assert.strictEqual(typeof jsonProperty['_ownerId'], 'string', 'Property _ownerId is string.');
+
             // title
+            assert.ok(jsonProperty.hasOwnProperty('title'), 'Property title exists.');
+            assert.strictEqual(typeof jsonProperty['title'], 'string', 'Property title is string.');
+
             // category
+            assert.ok(jsonProperty.hasOwnProperty('category'), 'Property category exists.');
+            assert.strictEqual(typeof jsonProperty['category'], 'string', 'Property category is string.');
+
             // maxLevel
+            assert.ok(jsonProperty.hasOwnProperty('maxLevel'), 'Property maxLevel exists.');
+            assert.strictEqual(typeof jsonProperty['maxLevel'], 'string', 'Property maxLevel is string.');
+
             // imageUrl
+            assert.ok(jsonProperty.hasOwnProperty('imageUrl'), 'Property imageUrl exists.');
+            assert.strictEqual(typeof jsonProperty['imageUrl'], 'string', 'Property imageUrl is string.');
+
             // summary
+            assert.ok(jsonProperty.hasOwnProperty('summary'), 'Property summary exists.');
+            assert.strictEqual(typeof jsonProperty['summary'], 'string', 'Property summary is string.');
+
             // createdOn -> check if exists and if it is a num
+            assert.ok(jsonProperty.hasOwnProperty('createdOn'), 'Property createdOn exists.');
+            assert.strictEqual(typeof jsonProperty['createdOn'], 'number', 'Property createdOn is string.');
+
             // id -> check if exists and if it is string
-
-            assert.ok(json.hasOwnProperty()
-
-            )
+            assert.ok(jsonProperty.hasOwnProperty('id'), 'Property id exists.')
+            assert.strictEqual(typeof jsonProperty['id'], 'string', 'Property id is a string');
         }
-    });
 
+        )
+    });
 });
+
